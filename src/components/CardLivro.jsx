d715a16aade13a0b717f8fa5989ef5e5f51937f9
famar584 <rm86802@fiap.com.br>
2023-09-12 22:25:20 -0300

    api

--- src/components/CardLivro.jsx
+++ src/components/CardLivro.jsx
@@ -5,7 +5,8 @@ import { useState } from 'react'
 
 export default function CardLivro({livro}){
     const [favorito, setFavorito] = useState(true)
-    const url_imagem = `https://image.tmdb.org/t/p/w200/${livro.poster_path}`
+    
+    const url_imagem = `https://storage.googleapis.com/du-prd/books/images/${livro.isbn13}`
 
     return (
         <div id="card" className='flex flex-col w-40 justify-center items-center m-2'>
@@ -18,9 +19,6 @@ export default function CardLivro({livro}){
             <span className='font-bold text-center line-clamp-1'>
                 {livro.title}
             </span>
-            <div>
-                <span>{livro.vote_average.toFixed(1)}</span>
-            </div>
             <a href="#" className='bg-pink-600 py-2 w-full rounded text-center'>
                 detalhes
             </a>
